22f129d4fde49ab3604412bafd30921b5f43907a
Aarni Koskela <akx@iki.fi>
2024-05-07 11:54:45 +0300

    chore: fix duplicate function names

--- backend/main.py
+++ backend/main.py
@@ -308,7 +308,7 @@ async def update_webhook_url(form_data: UrlForm, user=Depends(get_admin_user)):
 
 
 @app.get("/api/version")
-async def get_app_config():
+async def get_app_version():
     return {
         "version": VERSION,
     }
